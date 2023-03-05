05adf7502d11691dea03c6f23cb4b6a4ed37f49a
gooliajulia929 <juliadwyer93@gmail.com>
2023-03-05 16:43:11 +0100

    update and alphabetize meeting model

--- models/meeting.js
+++ models/meeting.js
@@ -6,11 +6,15 @@ const Meeting = new Schema(
     {
         attendees: [{
             member: { type: Schema.Types.ObjectId, ref: 'User' },
-            confirmed: { type: Boolean }
+            confirmed: { type: Boolean, required: true, default: false }
         }],
-        startTime: { type: String, enum: startTimeOptions },
+        description: { type: String },
         duration: { type: Number, enum: [30, 60, 90] },
+        // if meeting ids live on project, this isn't necessary
         project: { type: Schema.Types.ObjectId, ref: 'Project' },
+        recurring: { type: null || String, enum: ['mondays', 'tuesdays', 'wednesdays', 'thursdays', 'fridays', 'saturdays', 'sundays']},
+        startTime: { type: String, enum: startTimeOptions },
+        title: { type: String },
     },
     { timestamps: true }
 );
