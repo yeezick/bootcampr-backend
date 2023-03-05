0a265835c586bf6e844e38fdb1879323072e6f8b
gooliajulia929 <juliadwyer93@gmail.com>
2023-03-05 16:37:22 +0100

    update and alphabetize Project model

--- models/project.js
+++ models/project.js
@@ -4,12 +4,19 @@ const Schema = mongoose.Schema;
 
 const Project = new Schema(
     {
-        overview: { type: String, required: true },
-        members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-        title: { type: String, required: true, maxlength: 45 },
-        tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+        chats: [{ type: Schema.Types.ObjectId, ref: 'Chat' }],
+        // Add regex or some type check on the date format?
+        deadline: { type: String },
+        goal: { type: String, required: true },
         meetings: [{ type: Schema.Types.ObjectId, ref: 'Meeting' }],
-        chats: [{ type: Schema.Types.ObjectId, ref: 'Chat' }]
+        members: {
+            engineers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+            designers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+        },
+        problem: { type: String, required: true },
+        // Since each tasks holds 'status' info, is it really needed here as well?
+        tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }],
+        title: { type: String, required: true, maxlength: 45 },
         },
     { timestamps: true },
 );
