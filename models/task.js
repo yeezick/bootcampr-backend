5a13fbac61aa50dd9b135f2e8705c4b791baaffa
gooliajulia929 <juliadwyer93@gmail.com>
2023-03-05 16:28:40 +0100

    Update and alphabetize Task Model

--- models/task.js
+++ models/task.js
@@ -3,11 +3,17 @@ const Schema = mongoose.Schema;
 
 const Task = new Schema(
     {
-        title: { type: String },
-        description: { type: String },
-        status: { type: String, enum: ['to-do', 'in progress', 'in-review', 'completed', 'archived'], default: 'to-do' },
         assignee: { type: Schema.Types.ObjectId, ref: 'User' },
+        creator: { type: Schema.Types.ObjectId, ref: 'User' },
+        criteria: [{ type: String }],
+        description: { type: String },
+        priority: { type: String, enum: ['Urgent', 'High', 'Normal', 'Low'], default: 'Low'},
+        // If tasks live on a project, this isn't needed
         project: { type: Schema.Types.ObjectId, ref: 'Project' },
+        // NOTE: If we allow users to create their own column names, this enum needs adjustment
+        status: { type: String, enum: ['to-do', 'in progress', 'blocked', 'completed', 'archived'], default: 'to-do' },
+        type: { type: String, enum: ['bug', 'story', 'spike'] },
+        title: { type: String },
     },
     { timestamps: true }
 );
