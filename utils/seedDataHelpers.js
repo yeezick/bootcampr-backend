29897919efcb5291146687906eeb77578a375e48
gooliajulia929 <juliadwyer93@gmail.com>
2023-03-11 22:03:11 +0100

    clean up utilities

--- utils/seedDataHelpers.js
+++ utils/seedDataHelpers.js
@@ -2,37 +2,75 @@ import { faker } from '@faker-js/faker';
 import bcrypt from 'bcrypt';
 import Project from '../models/project.js';
 import User from '../models/user.js';
+import { generateRandomSingleDayAvailability } from './availability.js';
 
+// Either organize this file by User Utils, Project Utils, etc. 
+// OR design file structure to better seperate concerns
+
+/**
+ * Generate User Bio
+ * @param {enum: ['Software Engineer', 'UX Designer']} role 
+ * @returns {String} uniquely generated personal bio
+ */
 export const generateBio = (role) => {
-  return `I'm a ${faker.word.adjective()} ${role}. I'm an avid ${faker.word.noun()} lover and I'm passionate about ${faker.word.noun()}s. I'm ${faker.word.adjective()}, ${faker.word.adjective()}, and ${faker.word.adjective()} and look forward to using my skills to build something great!`;
+  const adjective1 = faker.word.adjective()
+  const preAdjective = ['a', 'e', 'i', 'o', 'u']
+    .includes(adjective1.charAt(0)) ? 'an' : 'a'
+
+  return [
+    `I'm ${preAdjective} ${adjective1} ${role}.`, 
+    `I'm an avid ${faker.word.noun()} lover and I'm passionate about ${faker.word.noun()}s.`,
+    `I'm ${faker.word.adjective()}, ${faker.word.adjective()}, and ${faker.word.adjective()}`,
+    `and look forward to using my skills to build something ${faker.word.adjective()}.`,
+    `Let's get ${faker.word.adjective()}!`
+  ].join(' ')
 };
 
+/**
+ * Generate Fake User
+ * @param {enum: ['Software Engineer', 'UX Designer']} role 
+ * @returns a User object with randomly generated user data
+ * 
+ */
 export const generateFakeUser = async (role) => {
   const name = {
     first: faker.name.firstName(),
     last: faker.name.lastName(),
   };
+
   return {
+    availability: {
+      sunday: JSON.stringify(generateRandomSingleDayAvailability()),
+      monday: JSON.stringify(generateRandomSingleDayAvailability()),
+      tuesday: JSON.stringify(generateRandomSingleDayAvailability()),
+      wednesday: JSON.stringify(generateRandomSingleDayAvailability()),
+      thursday: JSON.stringify(generateRandomSingleDayAvailability()),
+      friday: JSON.stringify(generateRandomSingleDayAvailability()),
+      saturday: JSON.stringify(generateRandomSingleDayAvailability())
+    },
     bio: generateBio(role),
-    declinedProjects: [],
     email: faker.helpers.unique(faker.internet.email, [name.first, name.last]),
     firstName: name.first,
-    githubUrl: `www.github.com/${name.first}-${name.last}`,
-    interestedProjects: [],
     lastName: name.last,
-    linkedinUrl: `www.linkedin.com/${name.first}-${name.last}`,
-    memberOfProjects: [],
-    ownerOfProjects: [],
+    links: {
+      githubUrl: `www.github.com/${name.first}-${name.last}`,
+      linkedinUrl: `www.linkedin.com/${name.first}-${name.last}`,
+      portfolioUrl: `www.${name.first}${name.last}.com`,
+    },
     passwordDigest: await bcrypt.hash('gumballs', 11),
-    portfolioProjects: [],
-    portfolioUrl: `www.${name.first}${name.last}.com`,
     profilePicture: faker.image.people(640, 480, true),
-    role: role,
-    savedProjects: [],
+    role,
     verified: true,
   };
 };
 
+/**
+ * Generate Multiple Fake Users
+ * @param {Number} quantity of users to create
+ * @param {enum: ['Software Engineer', 'UX Designer']} role type of Users to create
+ * @returns {Array<User>} an array of randomly generated users of indicated role type
+ * 
+ */
 export const generateFakeUsers = async (quantity, role) => {
   const users = [];
   for (let i = 0; i < quantity; i++) {
@@ -42,101 +80,71 @@ export const generateFakeUsers = async (quantity, role) => {
   return users;
 };
 
+/**
+ * Generate a Random Index
+ * @param {Number} length of array you need a random Index for
+ * @returns {Number} index to access random element of an array
+ * 
+ */
 export const randomIndex = (length) => Math.floor(Math.random() * length);
 
-export const roles = {
-  engineering: [
-    {
-      title: 'Testing Engineer',
-      description: 'thorough testing and maintaining high-quality product',
-      skills: ['Jest', 'Node.js', 'Express', 'React'],
-    },
-    {
-      title: 'DevOps Engineer',
-      description: 'cloud computing, database management and network management',
-      skills: ['AWS', 'Node.js', 'Kubernetes', 'Ansible', 'Docker'],
-    },
-    {
-      title: 'Frontend Software Engineer',
-      description: 'problem solving, constantly improving user experience, working collaboratively with designers',
-      skills: ['React', 'Figma', 'SCSS'],
-    },
-    {
-      title: 'Backend Software Engineer',
-      description: 'api development and management, writing clean and scalable code, and problem solving',
-      skills: ['Node.js', 'Express', 'MongoDB'],
-    },
-    {
-      title: 'Full Stack Software Engineer',
-      description:
-        'problem solving, excellent user-experience, collaborating with all types of engineers and designers',
-      skills: ['React', 'Figma', 'SCSS', 'Node.js', 'Express', 'MongoDB'],
-    },
-  ],
-  design: [
-    {
-      title: 'User Experience Researcher',
-      decription: 'leading researching, practicing empathy, and selecting appropriate methods of data collection',
-      skills: ['Maze', 'Userlytics', 'GSuite'],
-    },
-    {
-      title: 'Lead Designer',
-      decription:
-        'evolving designs to optimize user experience, working collaboratively with other designers and engineers, and managing a team of designers',
-      skills: ['Figma', 'Adobe Photoshop', 'Adobe Illustrator'],
-    },
-    {
-      title: 'Market Researcher + Designer',
-      description: 'analyzing market trends, designing with a user in mind and working collaboratively',
-      skills: ['Maze', 'Figma', 'Adobe Creative Cloud'],
-    },
-    {
-      title: 'Designer - Accessibility and Diversity Inclusion',
-      decription:
-        'developing products with all users in mind, advocating for a diverse user set and collaborating with other designers',
-      skills: ['All Together Now', 'Userlytics', 'Gsuite'],
-    },
-  ],
-};
-
-export const generateRole = (roleType) => {
-  return {
-    interestedApplicants: [],
-    status: ['Draft', 'Published'][Math.floor(Math.random() * 2)],
-    title: roleType.title,
-    description: `Looking for a ${roleType.title} who is interested in ${roleType.description}.`,
-    skills: [...roleType.skills],
-    maxHeadcount: Math.floor(Math.random() * 5) + 1,
-  };
-};
-
+/**
+ * Select a random subsection of an array
+ *  - Use case was to select random tools from array of project tools - not needed now?
+ * @param {Number} length of Array you'll be slicing
+ * @returns 2 Element array with starting and ending index for random slice
+ */
 export const randomArraySlice = (length) => {
   const startingIndex = Math.floor((Math.random() * length) / 2);
   const endingIndex = Math.floor(Math.random() * length + startingIndex);
   return [startingIndex, endingIndex];
 };
 
-export const generateFakeProject = (owner) => {
-  const durations = ['1 month', '6 weeks', '2 months', '3 months'];
-  const statuses = ['Draft', 'Published'];
-  const technologiesUsed = ['JavaScript', 'React', 'HTML', 'CSS', 'Rails', 'Ruby', 'Figma'];
+// Can be adapted as needed
+const defaultProject = {
+  title: 'Project Title',
+  goal: 'Project Goal',
+  problem: 'Project Problem',
+  startDate: Date.now(),
+  duration: 28,
+}
 
-  return new Project({
-    duration: durations[randomIndex(durations.length - 1)],
-    meetingCadence: Math.floor(Math.random() * 6 + 1),
-    overview: `A ${faker.word.adjective()} app to make ${faker.word.noun()}s more ${faker.word.adjective()}`,
-    projectOwner: owner,
-    roles: {
-      engineering: [generateRole(roles.engineering[randomIndex(roles.engineering.length)])],
-      design: [generateRole(roles.design[randomIndex(roles.design.length)])],
+// PROJECT UTILS
+/**
+ * Generate Project
+ * @param {Object} project custom options including: title, goal, problem, startDate and duration
+ * @returns 
+ */
+export const generateProject = (project = defaultProject) => {
+  const { title, goal, problem, startDate, duration } = project
+  // TODO: adjust start and end date format and calulcations as needed
+  const endDate = Date.now() + duration * 24 * 60 * 60 * 60
+
+  return {
+    chats: [],
+    goal,
+    meetings: [],
+    members: {
+      engineers: [],
+      designers: [],
     },
-    status: statuses[randomIndex(statuses.length - 1)],
-    technologiesUsed: [...technologiesUsed.slice(...randomArraySlice(technologiesUsed.length))],
-    title: `${faker.word.adjective()} ${faker.word.adjective()}`,
-  });
-};
+    problem,
+    tasks: [],
+    timeline: {
+      startDate,
+      endDate,
+    },
+    title,
+  }
+}
+
 
 // Fisher-Yates Shuffle
+/**
+ * Fisher-Yates Shuffle
+ * @param {Array} array 
+ * @returns same array with element order shuffled
+ */
 export const scrambleArrayOrder = (array) => {
   let currentIndex = array.length,
     randIndex;
@@ -149,6 +157,7 @@ export const scrambleArrayOrder = (array) => {
   return array;
 };
 
+// Tools bootcamprs may use for their projects - currently not used?
 export const tools = [
   {
     category: 'Engineering',
