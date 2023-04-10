09b61fdd47b7c66fd0a7907d6701449de32a3b65
Jason Martinez <jasonmar08@gmail.com>
2023-04-09 22:49:47 -0400

    incorporate chat models

---
+++ models/chat/groupChat.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const groupChat = new Schema(
+  {
+    groupName: { type: String, required: true },
+    groupDescription: { type: String, default: 'Group' },
+    groupPhoto: {
+      type: String,
+      default: 'https://i.postimg.cc/L8jC1rSp/default-group-avatar.png',
+    },
+    participants: [
+      {
+        participant: { type: mongoose.Types.ObjectId, ref: 'User' },
+        isAdmin: { type: Boolean, default: false, required: true },
+      },
+    ],
+    creator: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    typingUsers: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+    messages: [
+      {
+        text: { type: String },
+        sender: { type: mongoose.Types.ObjectId, ref: 'User' },
+        timestamp: { type: Date, default: Date.now },
+        status: {
+          type: String,
+          enum: ['sent', 'read', 'failed'],
+          default: 'sent',
+        },
+        readBy: [
+          {
+            user: { type: mongoose.Types.ObjectId, ref: 'User' },
+            timestamp: { type: Date, default: Date.now },
+          },
+        ],
+      },
+    ],
+    media: [{ type: mongoose.Types.ObjectId, ref: 'Media' }],
+    lastActive: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+);
+
+groupChat.pre('save', function (next) {
+  this.lastActive = new Date();
+  next();
+});
+
+groupChat.index({ groupName: 1, 'participants._id': 1 }, { unique: true });
+
+export default mongoose.model('GroupChat', groupChat);
---
+++ models/chat/privateChat.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const privateChat = new Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    typingUsers: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+    messages: [
+      {
+        text: { type: String },
+        sender: { type: mongoose.Types.ObjectId, ref: 'User' },
+        timestamp: { type: Date, default: Date.now },
+        status: {
+          type: String,
+          enum: ['sent', 'read', 'failed'],
+          default: 'sent',
+        },
+        readBy: [
+          {
+            user: { type: mongoose.Types.ObjectId, ref: 'User' },
+            timestamp: { type: Date, default: Date.now },
+          },
+        ],
+      },
+    ],
+    media: [{ type: mongoose.Types.ObjectId, ref: 'Media' }],
+    lastActive: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+);
+
+privateChat.pre('save', function (next) {
+  if (this.participants.length !== 2) {
+    const error = new Error('A private chat must have exactly 2 participants.');
+    next(error);
+  } else {
+    this.lastActive = new Date();
+    next();
+  }
+});
+
+privateChat.index({ participants: 1 });
+
+export default mongoose.model('PrivateChat', privateChat);
