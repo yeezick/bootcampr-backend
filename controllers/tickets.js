952272d31a8c211e2743af1994e9e3b13036f708
Bestnogm1 <koffiarielhessou@gmail.com>
2023-04-26 16:59:30 -0400

    full initergreated the backend for ticket

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -21,8 +21,19 @@ export const createTicket = async (req, res) => {
 export const ticketStatusHasNotChanged = async (req, res) => {
   try {
     console.log(req.body);
-
-    const ticket = await Ticket.findByIdAndUpdate(req.body.ticketId, req.body);
+    const { link, description, date, assignees, title } = req.body;
+    const ticket = await Ticket.findByIdAndUpdate(
+      req.body.ticketId,
+      {
+        description: description,
+        link: link,
+        date: date,
+        title: title,
+        assignees: assignees,
+      },
+      { new: true },
+    );
+    console.log(ticket);
     res.status(200).send(ticket);
   } catch (err) {
     console.log(err);
@@ -32,13 +43,14 @@ export const ticketStatusHasNotChanged = async (req, res) => {
 
 export const ticketStatusChanged = async (req, res) => {
   try {
-    const { link, newStatus, oldStatus, ticketId, projectId, description, date } = req.body;
+    const { link, newStatus, oldStatus, ticketId, projectId, description, date, assignees } = req.body;
     console.log(req.body);
     await Ticket.findByIdAndUpdate(ticketId, {
       status: newStatus,
       description: description,
       link: link,
       date: date,
+      assignees: assignees,
     });
 
     await Project.findByIdAndUpdate(projectId, {
@@ -54,11 +66,12 @@ export const ticketStatusChanged = async (req, res) => {
 };
 export const ticketDraggedToNewSection = async (req, res) => {
   try {
-    const { newStatus, oldStatus, ticketID, projectId } = req.body;
-    await Ticket.findByIdAndUpdate(ticketID, { status: newStatus });
+    const { newStatus, oldStatus, ticketId, projectId } = req.body;
+
+    await Ticket.findByIdAndUpdate(ticketId, { status: newStatus });
     await Project.findByIdAndUpdate(projectId, {
-      $pull: { [`projectTracker.${oldStatus}`]: ticketID },
-      $push: { [`projectTracker.${newStatus}`]: ticketID },
+      $pull: { [`projectTracker.${oldStatus}`]: ticketId },
+      $push: { [`projectTracker.${newStatus}`]: ticketId },
     });
 
     res.sendStatus(200);
@@ -67,15 +80,17 @@ export const ticketDraggedToNewSection = async (req, res) => {
     res.status(400).json({ message: 'Error updating ticket status.', error: err.message });
   }
 };
-const updateProject = async (req, res) => {
-  const project = Project.findById(req.body.ProjectId);
-};
 
 export const deleteTicket = async (req, res) => {
   try {
-    // deleteTicket.save();
-    await Project.findById(req.body.ProjectId);
-    res.status(200);
+    console.log(req.body);
+    const { ticketsStatus, ticketId, projectId } = req.body;
+
+    await Ticket.findOneAndRemove({ _id: ticketId });
+    await Project.findByIdAndUpdate(projectId, {
+      $pull: { [`projectTracker.${ticketsStatus}`]: ticketId },
+    });
+    res.sendStatus(200);
   } catch (error) {
     console.log(error);
     res.status(400).json({ message: 'Error deleting ticket.', error: error.message });
