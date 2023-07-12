08303baf451cdf23f14583be0a5d1e63e97b36cf
GooliaJulia <juliadwyer93@gmail.com>
2023-07-12 18:21:12 -0400

    return to split join approach after bug

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -2,5 +2,5 @@ export const formatCalendarId = (id) => `${id}@group.calendar.google.com`;
 
 // Converts query param attributes into the string format MongoDB expects
 export const convertQueryAttributesToMongoString = (attributes) => {
-    return attributes.replace(',', ' ')
+    return attributes.split(',').join(' ')
 }
