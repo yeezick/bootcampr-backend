11dd3ec4aa634b971c5d108d4fa0bfd31a71052d
GooliaJulia <juliadwyer93@gmail.com>
2023-08-02 17:35:40 -0400

    clean up comments controller formatting and console logs

--- controllers/task-management/comments.js
+++ controllers/task-management/comments.js
@@ -2,7 +2,7 @@ import Comment from '../../models/comment.js'
 import Ticket from '../../models/tickets.js'
 
 export const createComment = async (req, res) => {
-        try {
+    try {
         const { author, content, parentComment, ticketId, isReply } = req.body;
         const comment = await new Comment({ author, content, isReply });
         await comment.save();
@@ -10,14 +10,12 @@ export const createComment = async (req, res) => {
         if (parentComment) {
             await Comment.findByIdAndUpdate(parentComment, {$push: {"replies": comment._id}})
         }
-
-        const parentTicket = await Ticket.findByIdAndUpdate(ticketId, {$push: {"comments": comment._id}})
-
+        await Ticket.findByIdAndUpdate(ticketId, {$push: {"comments": comment._id}})
         res.status(200).json(comment)
-        } catch (err) {
-            console.error(err)
-            res.status(400).json({error: err.message })
-        }
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({error: err.message })
+    }
 };
 
 export const getAllComments = async (req, res) => {
@@ -32,20 +30,16 @@ export const getAllComments = async (req, res) => {
 
 export const getTicketComments = async (req, res) => {
     try {
+        let commentsWithData;
         const { ticketId } = req.params
+        const ticket = await Ticket.findById(ticketId)
 
-    const ticket = await Ticket.findById(ticketId)
-    console.log(ticket)
-
-    let commentsWithData;
-
-    if (ticket) {
-    commentsWithData = await Comment.find({_id: { $in: ticket.comments }})
-    } else {
-        throw Error('ticket not found')
-    }
-
-    res.status(200).json({comments: commentsWithData})
+        if (ticket) {
+            commentsWithData = await Comment.find({_id: { $in: ticket.comments }})
+        } else {
+            throw Error('ticket not found')
+        }
+        res.status(200).json({comments: commentsWithData})
     } catch (err) {
         console.error(err)
         res.status(400).json({error: err.message})
@@ -55,27 +49,29 @@ export const getTicketComments = async (req, res) => {
 
 export const deleteComment = async (req, res) => {
     try {
-      const { id } = req.params;
-      const deletedComment = await Comment.findByIdAndDelete(id);
-      if (deletedComment) {
-        return res.status(200).send('Comment deleted.');
-      }
-      throw new Error('Comment not found.');
+        const { id } = req.params;
+        const deletedComment = await Comment.findByIdAndDelete(id);
+
+        if (deletedComment) {
+            return res.status(200).send('Comment deleted.');
+        }
+        throw new Error('Comment not found.');
     } catch (error) {
-      console.error(error.message);
-      res.status(500).json({ error: error.message });
+        console.error(error.message);
+        res.status(500).json({ error: error.message });
     }
-  }; 
+}; 
 
 export const updateComment = async (req, res) => {
     try {
         const { commentId } = req.params;
         const comment = await Comment.findByIdAndUpdate(commentId, req.body, { new: true });
+
         if (!comment) {
             return res.status(404).json({ error: 'Comment not found.' });
         }
         res.status(200).send(comment);
-        } catch (error) {
+    } catch (error) {
         console.log(error.message);
         return res.status(404).json({ error: error.message });
     }
@@ -85,17 +81,13 @@ export const getReplies = async (req, res) => {
     try {
         const { commentId } = req.params
         const comment = await Comment.findById(commentId)
-        console.log(comment)
         const { replies } = comment
-
         const replyComments = await Comment.find({
             _id: { $in: replies }
         });
-        console.log(replyComments)
         res.status(200).json(replyComments)
     } catch (err) {
         console.error(err)
         res.status(400).json({error: err.message})
     }
 };
-
