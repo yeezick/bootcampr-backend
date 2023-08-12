235f9a2a594db9b6f168fd5d0500da55401f0727
Erick Manrique <ramerick5@gmail.com>
2023-08-12 14:56:53 -0500

    add static user and project to seed date

--- utils/seed/utils/helpers.js
+++ utils/seed/utils/helpers.js
@@ -1,9 +1,8 @@
-
 /**
  * Generate a Random Index
  * @param {Number} length of array you need a random Index for
  * @returns {Number} index to access random element of an array
- * 
+ *
  */
 export const randomIndex = (length) => Math.floor(Math.random() * length);
 
@@ -14,24 +13,24 @@ export const randomIndex = (length) => Math.floor(Math.random() * length);
  * @returns 2 Element array with starting and ending index for random slice
  */
 export const randomArraySlice = (length) => {
-    const startingIndex = Math.floor((Math.random() * length) / 2);
-    const endingIndex = Math.floor(Math.random() * length + startingIndex);
-    return [startingIndex, endingIndex];
+  const startingIndex = Math.floor((Math.random() * length) / 2);
+  const endingIndex = Math.floor(Math.random() * length + startingIndex);
+  return [startingIndex, endingIndex];
 };
 
 /**
  * Fisher-Yates Shuffle
- * @param {Array} array 
+ * @param {Array} array
  * @returns same array with element order shuffled
  */
 export const scrambleArrayOrder = (array) => {
-    let currentIndex = array.length,
-        randIndex;
-    
-        while (currentIndex != 0) {
-        randIndex = randomIndex(currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randIndex]] = [array[randIndex], array[currentIndex]];
-    }
-    return array;
+  let currentIndex = array.length,
+    randIndex;
+
+  while (currentIndex != 0) {
+    randIndex = randomIndex(currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randIndex]] = [array[randIndex], array[currentIndex]];
+  }
+  return array;
 };
