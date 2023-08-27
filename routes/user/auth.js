e0eb604dc840ccccb9deda876cec4dc15fa0db12
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2023-08-27 00:03:19 -0400

    rebase problems...thanks Eric u jinxed me lol

--- routes/user/auth.js
+++ routes/user/auth.js
@@ -1,10 +1,5 @@
 import { Router } from 'express';
-import {
-  resendNewEmailLink,
-  verifyEmailLink,
-  verifyUniqueEmail,
-  verifyValidToken,
-} from '../../controllers/auth/emailVerification.js';
+import { resendNewEmailLink, sendUpdateEmailVerificationEmail, verifyEmailLink, verifyUniqueEmail } from '../../controllers/auth/emailVerification.js';
 import { signUp, signIn, verify, confirmPassword, updatePassword } from '../../controllers/auth/auth.js';
 
 //middleware
@@ -15,6 +10,7 @@ router.post('/sign-in', signIn);
 router.get('/verify', verify);
 router.get('/verify-email/:email', verifyUniqueEmail);
 router.post('/users/:id/expired-link', resendNewEmailLink);
+router.post('/users/:id/update-email-verification', sendUpdateEmailVerificationEmail)
 router.get('/:id/verify/:token', verifyEmailLink);
 router.get('/verify-token-expiration/:emailToken', verifyValidToken);
 router.post('/confirm-password/:userID', confirmPassword);
