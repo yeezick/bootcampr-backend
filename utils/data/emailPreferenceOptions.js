37932a109da573e5098321e36cf645dfe6477311
GooliaJulia <juliadwyer93@gmail.com>
2023-11-08 17:56:47 -0700

    create source of truth for email options and descriptions

---
+++ utils/data/emailPreferenceOptions.js
@@ -0,0 +1,34 @@
+/**
+ * Source of truth for Email Preference options and corresponding user friendly copy
+ */
+
+export const emailPreferenceOptions = {
+    projectUpdates: {
+        title: 'Project updates',
+        description: 'Anything project related. This includes meeting invites and task management ticket updates.'
+    },
+    chatNotifications: {
+        title: 'Team chat notifications',
+        description: "You'll be notified when team members send messages."
+    },
+    surveysAndFeedback: {
+        title: 'Surveys and feedback requests',
+        description: 'We love feedback and may ask you how we can improve to better help you.'
+    },
+    bootcamprUpdates: {
+        title: 'Bootcampr updates',
+        description: "Stay up-to-date on new and improved features as they're released!"
+    },
+    eventInvitations: {
+        title: 'Event invitations',
+        description: "We'll invite you to virtual speaking events with professional UX Designers, Software Engineers, and Product Managers"
+    },
+    newsletters: {
+        title: 'Newsletters',
+        description: "Look out for Bootcampr's newsletter coming soon."
+    },
+    jobAlerts: {
+        title: 'Job alerts',
+        description: "We're excited to send job alerts in the future."
+    }
+}
