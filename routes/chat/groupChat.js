fd896e96a1d3dfa539349761982e7b82fc1b7d8e
ozge <ozgecaylioglu@gmail.com>
2024-01-02 21:20:02 -0500

    refactor group chat routes

--- routes/chat/groupChat.js
+++ routes/chat/groupChat.js
@@ -2,34 +2,37 @@ import { Router } from 'express';
 import {
   createGroupChatMessage,
   createGroupChatRoom,
-  deleteGChat,
+  deleteGroupChat,
   deleteGroupChatThread,
   deleteMessage,
-  getAllGroupChatsByUserId,
-  getAllGroupMessages,
+  getUserGroupChats,
+  getGroupChatMessages,
   getAllUsersGroupChats,
   getGroupChatByChatId,
   leaveGroupChat,
   updateGroupChatInfo,
 } from '../../controllers/chat/groupChat.js';
-import { createMediaMessage, getChatMediaByFileType, getMediaByChatId } from '../../controllers/chat/media.js';
+import { createMediaMessage, getChatMediaByFileType, getMediaMessage } from '../../controllers/chat/media.js';
 const router = Router();
 
 router.get('/groupChats', getAllUsersGroupChats);
-router.post('/:userId/groupChats', createGroupChatRoom);
-router.post('/:userId/groupChats/:groupChatId', createGroupChatMessage);
-router.get('/:userId/groupChats', getAllGroupChatsByUserId);
-router.get('/:userId/groupChats/:groupChatId', getAllGroupMessages);
 router.get('/groupChats/:groupChatId', getGroupChatByChatId);
-router.put('/:userId/groupChats/:groupChatId', updateGroupChatInfo);
-router.delete('/:userId/groupChats/:groupChatId', deleteGroupChatThread);
-router.put('/:userId/:groupChatId', leaveGroupChat);
-router.delete('/groupChats/:chatId', deleteGChat);
+router.get('/groupChats/:groupChatId/messages', getGroupChatMessages);
+router.delete('/groupChats/:groupChatId', deleteGroupChat);
 router.delete('/groupChats/:groupChatId/messages', deleteMessage);
+//user's group chats
+//ASK - instead of getting user id from endpoint we can get it from token to avoid  /users/userId/chats/id etc.. but not sure which is better
+router.get('/user/groupChats', getUserGroupChats);
+router.post('/user/groupChats', createGroupChatRoom);
+router.post('/user/groupChats/:groupChatId/messages', createGroupChatMessage);
+router.put('/user/groupChats/:groupChatId', updateGroupChatInfo);
+//ASK - not sure about this endpoint, we're extracting userId from headers, also should we use delete instead of put?
+router.put('/groupChats/:groupChatId/participants/me', leaveGroupChat);
+router.delete('/user/groupChats/:groupChatId', deleteGroupChatThread);
 
 // Media Messages
-router.post('/:userId/groupChats/:groupChatId/media', createMediaMessage);
-router.get('/groupChats/:groupChatId/media', getMediaByChatId);
-router.get('/groupChats/:groupChatId/media/:fileType', getChatMediaByFileType);
+router.get('/groupChats/:groupChatId/media', getMediaMessage);
+router.get('/groupChats/:groupChatId/media?type=fileType', getChatMediaByFileType);
+router.post('/user/groupChats/:groupChatId/media', createMediaMessage);
 
 export default router;
