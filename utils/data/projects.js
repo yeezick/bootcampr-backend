e7ec4a91393c93e5292218fdb3467ae57cb9f6be
GooliaJulia <juliadwyer93@gmail.com>
2024-02-21 21:24:52 -0700

    Remove startDate from default project data and let generateProject determine

--- utils/data/projects.js
+++ utils/data/projects.js
@@ -3,6 +3,5 @@ export const defaultProject = {
     title: 'Travel Troubles',
     goal: 'Design & ship a responsive website.',
     problem: 'How might we connect people with similar travel plans/interests?',
-    startDate: Date.now(),
     duration: 28,
 }
