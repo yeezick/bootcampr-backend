990234279f95e060accd1353f71a823efc4d5198
Felix Owolabi <owolabifelix78@gmail.com>
2024-02-23 17:07:11 +0100

    included error handling in global setup and teardown file

--- jest.teardown.js
+++ jest.teardown.js
@@ -2,11 +2,15 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import config from './config.js';
 
 const globalTeardown = async () => {
-  if (config.Memory) {
-    const instance = global.__MONGOINSTANCE__;
-    await instance.stop();
+  try {
+    if (config.Memory) {
+      const instance = global.__MONGOINSTANCE__;
+      await instance.stop();
+    }
+  } catch (error) {
+    console.error('Error during global teardown:', error);
+    process.exit(1);
   }
 };
 
 export default globalTeardown;
-
