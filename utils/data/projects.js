f0db3c6c478d3216af4f21c27ed6fe26a888f7d4
Jordan Jennings <71848547+jordanje@users.noreply.github.com>
2024-02-28 20:43:56 -0500

    feature/bc-746-adding-week-and-year (#122)
    In this PR:
    - updated duration to project start and end dates are 28 days apart
    - updated time format for project start end end dates for Full Calendar
    
    ---------
    
    Co-authored-by: Automated Version Bump <gh-action-bump-version@users.noreply.github.com>

--- utils/data/projects.js
+++ utils/data/projects.js
@@ -3,5 +3,5 @@ export const defaultProject = {
     title: 'Travel Troubles',
     goal: 'Design & ship a responsive website.',
     problem: 'How might we connect people with similar travel plans/interests?',
-    duration: 28,
+    duration: 27,
 }
