ad8d1b3123291fe236f1757e636af9f523da7f00
Jordan Jennings <jordanjennings12@gmail.com>
2024-03-20 15:02:12 -0400

    commented out projectevents

--- utils/projectEvents.js
+++ utils/projectEvents.js
@@ -16,6 +16,7 @@ export const createGoogleEvent = async (eventInfo) => {
   };
 
   const { data: googleEvent } = await calendar.events.insert(preparedEvent);
+  console.log(googleEvent)
   const convertedEvent = convertGoogleEventsForCalendar([googleEvent]);
   return convertedEvent
 }
